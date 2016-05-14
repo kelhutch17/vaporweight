a47e3615f5b89ee2eccd29a0bca783cd324ffa8b
Kelly Hutchison <kelhutch@verizon.net>
2016-05-14 15:57:03 -0700

    initial commit

---
+++ vaporweight/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  vaporweight
+//
+//  Created by Kelly Hutchison on 5/14/16.
+//  Copyright © 2016 Kelly Hutchison. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
